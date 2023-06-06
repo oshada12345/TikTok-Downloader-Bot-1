37395fa797809942667f792aaa493536c0cfd518
oshada12345 <135181748+oshada12345@users.noreply.github.com>
2023-06-06 09:37:39 +0530

    Update url_filter.py

--- bot/filters/url_filter.py
+++ bot/filters/url_filter.py
@@ -6,7 +6,7 @@ from aiogram.types import Message
 class UrlFilter(BaseFilter):
     """Url Filter."""
     async def __call__(self, message: Message) -> bool:
-        if message.text.startswith("https://www.tiktok.com/") 
+        if message.text.startswith("https://www.tiktok.com/")/
         or message.text.startswith("https://vt.tiktok.com/"):
             return True
         
