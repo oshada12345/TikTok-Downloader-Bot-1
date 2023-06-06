f0aeb06a701a21c756e0d1b595420b7824f26bb3
oshada12345 <135181748+oshada12345@users.noreply.github.com>
2023-06-06 09:17:38 +0530

    Update url_filter.py

--- bot/filters/url_filter.py
+++ bot/filters/url_filter.py
@@ -10,7 +10,7 @@ class UrlFilter(BaseFilter):
         or message.text.startswith("https://vt.tiktok.com/"):
             return True
         
-        await message.answer('💀👹  ώέ𝕃ⒸỖｍ𝐞 т๏ 𝔱Ⓘк 𝔱ØＫ 𝓭𝓞Ŵｎ𝔩σ卂𝕕𝕖𝓡 𝐛𝕆Ⓣ  🔥☆/n/nᴘʟᴢ ꜱᴇɴᴅ ᴍᴇ ᴠɪᴅᴇᴏ ʟɪɴᴋ'),
+        await message.answer(' 🔥☆ ᴘʟᴢ ꜱᴇɴᴅ ᴍᴇ ᴠɪᴅᴇᴏ ʟɪɴᴋ'),
         await message.reply_sticker('CAACAgUAAxkBAAED9kRiDq_GkOHuRHPeVv4IRhsvy4NtbwACqQQAAncUyFftN80YUiyXnyME')
 
         return False
