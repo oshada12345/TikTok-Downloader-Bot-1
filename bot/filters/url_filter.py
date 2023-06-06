42ee361f77c30692a7ce5a45514229dd0d15f847
oshada12345 <135181748+oshada12345@users.noreply.github.com>
2023-06-06 17:33:23 +0530

    Update url_filter.py

--- bot/filters/url_filter.py
+++ bot/filters/url_filter.py
@@ -7,7 +7,7 @@ class UrlFilter(BaseFilter):
     """Url Filter."""
     async def __call__(self, message: Message) -> bool:
         if message.text.startswith("https://www.tiktok.com/") \
-        or message.text.startswith("https://vt.tiktok.com , https://m.tiktok.com"):
+        or message.text.startswith("https://vt.tiktok.com/"):
             return True
         
         await message.answer('ᴘʟᴢ ꜱᴇɴᴅ ᴍᴇ ᴠɪᴅᴇᴏ ʟɪɴᴋ'),
