ffa4ae87ee1aa62738923da9a158a0cfa0395108
oshada12345 <135181748+oshada12345@users.noreply.github.com>
2023-06-06 18:02:59 +0530

    Update tiktok.py

--- bot/ext/tiktok.py
+++ bot/ext/tiktok.py
@@ -32,7 +32,7 @@ class TikTok:
             "TE": "trailers"
         }
 
-        self.server_url = "https://musicaldown.com/en"
+        self.server_url = "https://musicaldown.com/"
         self.post_url = self.server_url + "id/download"
 
         self.session = requests.Session()
