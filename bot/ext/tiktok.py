ecec3f91a7b0b7ef86f0b75f82beac2dcef470e8
oshadaXD <135181748+oshada12345@users.noreply.github.com>
2023-06-18 08:03:42 +0530

    Update tiktok.py

--- bot/ext/tiktok.py
+++ bot/ext/tiktok.py
@@ -19,7 +19,7 @@ class TikTok:
     def __init__(self, url: str) -> None:
         self.url = url
         self.headers = {
-            "Host": "musicaldown.com",
+            "Host": ",
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:103.0) Gecko/20100101 Firefox/103.0",
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
             "Accept-Language": "en-US,en;q=0.5",
@@ -32,7 +32,7 @@ class TikTok:
             "TE": "trailers"
         }
 
-        self.server_url = "https://musicaldown.com/"
+        self.server_url = "https://ssyoutube.com/"
         self.post_url = self.server_url + "id/download"
 
         self.session = requests.Session()
